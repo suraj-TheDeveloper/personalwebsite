fafb584a077fe5baf16c3e427c681998d38a6c73
suraj-TheDeveloper <thedevelopersuraj@gmail.com>
2021-02-14 15:39:07 +0530

    Footer file

---
+++ footer.php
@@ -0,0 +1,15 @@
+<!-- footer section -->
+<div id="contact">
+    <div class="container">
+        <center>
+            <img src="/personalsite/title.png" width="200" height="200">
+            <h3 class="email">Follow Me On</h3>
+            <a href="https://www.instagram.com/suraj_thedeveloper/"><img src="/personalsite/insta.png" width="20" height="20"></a>
+            <a href="https://www.linkedin.com/in/suraj-kumar-88329a184"><img src="/personalsite/Linkedin-logo.png" width="30" height="30"></a>
+            <a href="https://t.me/joinchat/VVr86t_v28XPZjiE"><img src="/personalsite/telegram.png" width="20" height="20"></a>
+            <a href="https://youtube.com/channel/UCq44E-HDYZwdysceXUDFv4A"><img src="/personalsite/youtube.png" width="30" height="30"></a>
+            <h6 class="copyright">Copyright <i class="fas fa-copyright"></i> Design and Developed by Suraj kumar</h6>
+        </center>
+    </div>
+</div>
+<!-- end -->
