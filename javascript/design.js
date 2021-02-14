2c5892ec0d930876f5312459cda621badb632c41
suraj-TheDeveloper <thedevelopersuraj@gmail.com>
2021-02-14 15:37:00 +0530

    javascript files

---
+++ javascript/design.js
@@ -0,0 +1,17 @@
+//index page
+$(document).ready(function(){
+    $(".site-title-first-mobile").fadeOut(1000);
+    $(".site-title-first-mobile").fadeIn(3000);
+});
+$(document).ready(function(){
+    $(".site-title-middle-mobile").fadeOut(1000);
+    $(".site-title-middle-mobile").fadeIn(6000);
+});
+//Question and Answer page
+$(document).ready(function(){
+    $(".top-head").fadeOut(1000);
+    $(".top-head").fadeIn(6000);
+    $(".head-explain").fadeOut(1000);
+    $(".head-explain").fadeIn(6000);
+});
+
