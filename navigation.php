50de5b8dc86941ad1c99fb65b9a0633ae76c4611
suraj-TheDeveloper <thedevelopersuraj@gmail.com>
2021-02-14 15:40:03 +0530

    navigation file

---
+++ navigation.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-sm navbar-expand-xl navbar-expand-md navbar-dark bg-dark fixed-top shadow-lg container-fluid">
+    <a href="#" style="font-family: mon; font-size: 20px;" class="navbar-brand">Suraj TheDeveloper</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item nav-link-right">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#about">About Me</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#cv">Curriculam Vitae</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#contact">Contact Me</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+            
