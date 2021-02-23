1d448ca1ee22f1d9b68abd16c2934127e6f0d660
suraj-TheDeveloper <thedevelopersuraj@gmail.com>
2021-02-23 15:59:25 +0530

    index

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
         ?>
     </head>
     <style>
+       
         h2{
             color: white;
         }
@@ -34,11 +35,21 @@
         }
     </style>
     <body>
+        <?php
+            $link = mysqli_connect("localhost", "root", "", "personal");
+            /** selecting the indexpage table */
+            $Sql = "SELECT * FROM indexpage";
+            /**now executing the query */
+            $result = mysqli_query($link, $Sql);
+            /** now display */
+            while($row = mysqli_fetch_array($result)){
+
+        ?>
         <!-- head section -->
         <div id="home">
             <div class="container container-fluid">
-                <h2 class="site-title-first-mobile site-title-first-tablet site-title-first-laptop">Suraj Kumar. L. N. S. K </h2>
-                <h2 class="site-title-middle-mobile site-title-middle-tablet site-title-middle-laptop">Web Design and Developer</h2>
+                <h2 class="site-title-first-mobile site-title-first-tablet site-title-first-laptop"><?php echo $row['Sitetitle']; ?></h2>
+                <h2 class="site-title-middle-mobile site-title-middle-tablet site-title-middle-laptop"><?php echo $row['Subtitle']; ?></h2>
             </div>
         </div>
         <!-- end -->
@@ -46,74 +57,104 @@
         <div id="about">
             <div class="container">
                 <center><h3 class="h2 about">About Me</h3></center>
-                <img src="/personalwebsite/images/pictures/titleimage.png" width="330" height="300" style="float: left;">
+                <img src="/personalwebsite/images/pictures/titleimage.png" width="300" height="300" style="float: left;">
                 <p class="about-para">
-                    I love to develop a site from scratch and design a site according to 
-                    customer needs. I started my career on july 1 2019 with the knowledge of 
-                    HTML and CSS only. Later i studied JavaScript, MySQL, Bootstrap, PHP, 
-                    Angular JS, WordPress. As a Web Developeri love to use Linux Instead Of
-                    Windows because of its popularity among developers.
+                   <?php echo $row['Aboutme']; ?>
                 </p>
                 <h3 class="h3" style="font-family: 'Noto Serif', serif;">About My Intrest</h3>
                 <p class="about-para">
-                    I love to study deeply about a technology and create a product using
-                    that technology. So i love to learn the technologies and create a product 
-                    which will chage the world. This is the first step i have taken for my 
-                    career by learning the web design and development and slowly entering 
-                    into server and cloud technology. You can see my tutorials in Youtube My 
-                    Channel name is Suraj TheDeveloper.
+                   <?php echo $row['Aboutmyintrest']; ?>
                 </p>
             </div>
         </div>
         <!-- end -->
+        <?php
+            }
+            $links = mysqli_connect("localhost", "root", "", "personal-cv");
+            /** selecting the indexpage table */
+            $Sql = "SELECT * FROM careerobjective";
+            /**now executing the query */
+            $result = mysqli_query($links, $Sql);
+            /** now display */
+            while($row = mysqli_fetch_array($result)){
+
+        ?>
         <!-- my cv section -->
         <div id="cv">
             <div class="container">
                 <center><h3 class="h2 about">Curriculam Vitae - (CV)</h3></center>
                 <h3 class="h3" style="font-family: monlight; margin-top: 30px;">Career Objective:</h3>
                 <p class="about-para">
-                    To make more and more products that will reach the worldwide. To develop a 
-                    product which will be more useful that it will increase the company standard.
-                    To implement the concepts that I have learnt and prove my skills to the world.
+                    <?php echo $row['objective']; ?>
                 </p>
                 <h3 class="h3" style="font-family: monlight; margin-top: 30px;">Education Qualification:</h3>
+                <?php
+            }
+            $links = mysqli_connect("localhost", "root", "", "personal-cv");
+            $Sql = "SELECT * FROM qualification";
+            /**now executing the query */
+            $result = mysqli_query($links, $Sql);
+            /** now display */
+            while($row = mysqli_fetch_array($result)){
+                ?>
                 <p class="about-para">
-                   -> 10th Graduation under Mahatma Montessori Matriculation Higher Secondary School with percentage of 91.5 – 2016<br>
-                   -> 12th Graduation under Mahatma Montessori Matriculation Higher Secondary School with percentage of 66 – 2018
+                   -> <?php echo $row['Qualification']; ?>
                 </p>
+                <?php
+            }
+                ?>
                 <h3 class="h3" style="font-family: monlight; margin-top: 30px;">Certificates:</h3>
+                <?php
+                    $links = mysqli_connect("localhost", "root", "", "personal-cv");
+                    $Sql = "SELECT * FROM certificates";
+                    $result = mysqli_query($links, $Sql);
+                    while($row = mysqli_fetch_array($result)){
+                ?>
                 <p class="about-para">
-                    1. Ubuntu Linux Desktop Basics – Udemy.<br>
-                    2. Ubuntu Web Development Setup – Udemy.<br>
-                    3. Mastering Linux Command Line – Udemy.<br>
-                    4. Ubuntu Linux Fundamentals Learn Linux Server with ubuntu - Udemy.<br>
-                    5. Web Designing Certificate Course at ACE Training.<br>
-                    6. Angular Js and WordPress Certificate Course at ACE Training.<br>
-                    7. Participated in Web Design competition and won 3rd prize conducted at SLCS College.<br>
-                    8. Workshop on Web Programming using PHP conducted at SLCS College.<br>
+                        <?php echo $row['id']; ?>.<?php echo $row['certificates']; ?>
                 </p>
+                <?php
+                }
+                ?>
                 <h3 class="h3" style="font-family: monlight; margin-top: 30px;">Languages Known:</h3>
+                <?php
+                    $links = mysqli_connect("localhost", "root", "", "personal-cv");
+                    $Sql = "SELECT * FROM languages";
+                    $result = mysqli_query($links, $Sql);
+                    while($row = mysqli_fetch_array($result)){
+                ?>
                 <p class="about-para">
-                    1. PHP<br>
-                    2. MYSQL<br>
-                    3. HTML<br>
-                    4. CSS<br>
-                    5. JavaScript<br>
-                    6. Ajax<br>
+                   <?php echo $row['id']; ?>.<?php echo $row['languages']; ?>
                 </p>
+                <?php
+                    }
+                ?>
                 <h3 class="h3" style="font-family: monlight; margin-top: 30px;">Frameworks Known:</h3>
+                <?php
+                    $links = mysqli_connect("localhost", "root", "", "personal-cv");
+                    $Sql = "SELECT * FROM framesworks";
+                    $result = mysqli_query($links, $Sql);
+                    while($row = mysqli_fetch_array($result)){
+                ?>
                 <p class="about-para">
-                    1. Bootstrap (CSS Framework).<br>
-                    2. Angular JS (JavaScript Framework).<br>
-                    3. JQUERY (JavaScript Library).<br>
-                    4. WordPress (Opensource Content Management System (CMS)).<br>
+                    <?php echo $row['id']; ?>.<?php echo $row['frameworks']; ?>
                 </p>
-                <h3 class="h3" style="font-family: monlight; margin-top: 30px;">Technical Skills:</h3>
+                <?php
+                    }
+                ?>
+                 <h3 class="h3" style="font-family: monlight; margin-top: 30px;">Technical Skills:</h3>
+                <?php
+                    $links = mysqli_connect("localhost", "root", "", "personal-cv");
+                    $Sql = "SELECT * FROM skills";
+                    $result = mysqli_query($links, $Sql);
+                    while($row = mysqli_fetch_array($result)){
+                ?>
                 <p class="about-para">
-                    1. Knowledge in Linux Ubuntu basics.<br>
-                    2. Knowledge in Linux Command Line.<br>
-                    3. Design and Develop a web site.<br>
+                    <?php echo $row['id']; ?>.<?php echo $row['skills']; ?>
                 </p>
+                <?php
+                    }
+                ?>
             </div>
         </div>
         <?php
