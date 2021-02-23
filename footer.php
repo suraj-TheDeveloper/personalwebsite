cbb4ef03a84e7ac2483349a5bdb0194012e33c4b
suraj-TheDeveloper <thedevelopersuraj@gmail.com>
2021-02-23 15:59:12 +0530

    footer

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
             <a href="https://www.linkedin.com/in/suraj-kumar-88329a184"><img src="/personalsite/Linkedin-logo.png" width="30" height="30"></a>
             <a href="https://t.me/joinchat/VVr86t_v28XPZjiE"><img src="/personalsite/telegram.png" width="20" height="20"></a>
             <a href="https://youtube.com/channel/UCq44E-HDYZwdysceXUDFv4A"><img src="/personalsite/youtube.png" width="30" height="30"></a>
-            <h6 class="copyright">Copyright <i class="fas fa-copyright"></i> Design and Developed by Suraj kumar</h6>
+            <h6 class="copyright">Copyright <i class="fas fa-copyright"></i> 2020 - <?php echo date("Y"); ?> Design and Developed by Suraj kumar</h6>
         </center>
     </div>
 </div>
