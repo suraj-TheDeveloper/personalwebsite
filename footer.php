b1e3cb347509e897fa341d24ec2e642557644864
suraj-TheDeveloper <thedevelopersuraj@gmail.com>
2022-01-09 12:15:31 +0530

    Footer code

--- footer.php
+++ footer.php
@@ -2,12 +2,12 @@
 <div id="contact">
     <div class="container">
         <center>
-            <img src="/personalsite/title.png" width="200" height="200">
+            <img src="/personalwebsite/images/pictures/title.png" width="200" height="200">
             <h3 class="email">Follow Me On</h3>
-            <a href="https://www.instagram.com/suraj_thedeveloper/"><img src="/personalsite/insta.png" width="20" height="20"></a>
-            <a href="https://www.linkedin.com/in/suraj-kumar-88329a184"><img src="/personalsite/Linkedin-logo.png" width="30" height="30"></a>
-            <a href="https://t.me/joinchat/VVr86t_v28XPZjiE"><img src="/personalsite/telegram.png" width="20" height="20"></a>
-            <a href="https://youtube.com/channel/UCq44E-HDYZwdysceXUDFv4A"><img src="/personalsite/youtube.png" width="30" height="30"></a>
+            <a href="https://www.instagram.com/suraj_thedeveloper/"><img src="/personalwebsite/images/pictures/insta.png" width="20" height="20"></a>
+            <a href="https://www.linkedin.com/in/suraj-kumar-88329a184"><img src="/personalwebsite/images/pictures/Linkedin-logo.png" width="30" height="30"></a>
+            <a href="https://t.me/joinchat/VVr86t_v28XPZjiE"><img src="/personalwebsite/images/pictures/telegram.png" width="20" height="20"></a>
+            <a href="https://youtube.com/channel/UCq44E-HDYZwdysceXUDFv4A"><img src="/personalwebsite/images/pictures/youtube.png" width="30" height="30"></a>
             <h6 class="copyright">Copyright <i class="fas fa-copyright"></i> 2020 - <?php echo date("Y"); ?> Design and Developed by Suraj kumar</h6>
         </center>
     </div>
